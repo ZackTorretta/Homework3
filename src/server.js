77d4c1e8de1f6223e12ecbe08d12fa836accd75b
ZackTorretta <ztorretta@ltu.edu>
2021-03-28 19:07:11 -0400

    2

--- src/server.js
+++ src/server.js
@@ -1,15 +1,9 @@
 const express = require('express');
 
 const app = express();
-const dateOb = new Date();
 
 app.get('/', (req, res) => {
-  const hours = dateOb.getHours();
-  const objOne = JSON.stringify(hours);
-  const minutes = dateOb.getMinutes();
-  const objTwo = JSON.stringify(minutes);
-  const time = `${objOne}:${objTwo}`;
-  res.send(time);
+  res.send();
 });
 
 app.listen(8080);
