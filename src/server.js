7cad8ffb6b7aa9438ae66192a3d88648561f3b9b
ZackTorretta <ztorretta@ltu.edu>
2021-03-28 19:38:20 -0400

    2

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const BodyParser = require('body-parser');
 const Mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 app.use(BodyParser.json());
@@ -16,4 +17,13 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.send('hello world');
 });
-app.listen(8080);
+
+(async () => {
+  await Mongoose.connect('mongodb+srv://ADMIN:ztorre97@cluster0.doojn.mongodb.net/Cluster0?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+  app.listen(8080);
+})();
