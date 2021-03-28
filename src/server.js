010b7024ba8134b29f6f880156588defd2a297d4
ZackTorretta <ztorretta@ltu.edu>
2021-03-28 19:30:32 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const BodyParser = require('body-parser');
+const Mongoose = require('mongoose');
 
 const app = express();
 
+/* app.use((req, res, next) => {
+  console.log('hello');
+  req.whatever = 123;
+  next();
+});
 app.use((req, res, next) => {
-  res.send();
+  console.log(req.whatever);
+  next();
+}); */
+app.get('/', (req, res) => {
+  res.send('hello world');
 });
 app.listen(8080);
